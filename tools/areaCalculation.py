4b36f554b4513c76ba626a591132db75f1714b7c
Jerick Seng <64419693+jer123se12@users.noreply.github.com>
2022-01-12 21:13:11 +0800

    completed area of triangle
    err extention-> add solve for all the values of the triangle

--- tools/areaCalculation.py
+++ tools/areaCalculation.py
@@ -3,15 +3,19 @@ import math
 def triangle(base, height):
     return 1/2 * base * height
 
+def sine_rule_side(a,A,b):
+    return math.sin(b)*(A/math.sin(a))
+
 def solve_triangle(A,B,C,a,b,c):
-    if (A=="?"+B=="?"+C=="?"+a=="?"+b=="?"+c=="?")<3:
+    if ((A!="?")+(B!="?")+(C!="?")+(a!="?")+(b!="?")+(c!="?"))<3:
         return "not possible"
     #everything in radians btw, cuz math uses radians
     # if the value is "?" then is unknown
-    if (A=="?"+B=="?"+C=="?") == 3:
-        c=math.acos( ( a**2 + b**2﹣c**2 )/( 2*a*b ) )
-        
-    elif (A=="?"+B=="?"+C=="?") == 2:
+    if ((A!="?")+(B!="?")+(C!="?")) == 3:
+        s=(A+B+C)/2
+        Area=math.sqrt(s*(s-a)*(s-b)*(s-c))
+        return Area
+    elif ((A!="?")+(B!="?")+(C!="?")) == 2:
         if A=="?":
             #need to check if this is copied or refrenced
             A=C
@@ -26,7 +30,8 @@ def solve_triangle(A,B,C,a,b,c):
             b=d
         # A, B is known, C is not known
         if c!="?":
-            Area=1/2*A*B*math.sin(c) 
+            Area=1/2*A*B*math.sin(c)
+            return Area
             #need figure out the rest of them(A,B,c) is known
         else:
             if b=="?":
@@ -36,7 +41,33 @@ def solve_triangle(A,B,C,a,b,c):
                 c=(math.pi-b)-(math.asin(A*(math.sin(b)/B)))
                 return solve_triangle(A,B,C,a,b,c)
     else:
-        
+        # there are at least 2 angles
+        #lets make find all the angles
+        if a=="?":
+            print(a,b,c)
+            a=(180-b)-c
+        elif b=="?":
+            b=(180-a)-c
+        else:
+            c=(180-b)-a
+        #now all angles are known
+        if A=="?" and B=="?" and C=="?":
+            return "not possible, there are multiple triangles"
+        else:
+            #solve using SAS
+            #i mean solve all sides
+            if A!="?":
+                B=sine_rule_side(a,A,b)
+                C=sine_rule_side(a,A,c)
+            elif B!="?":
+                A=sine_rule_side(b,B,a)
+                C=sine_rule_side(b,B,c)
+            else:
+                A=sine_rule_side(c,C,a)
+                B=sine_rule_side(c,C,b)
+            #now we know all
+            Area=1/2*A*B*math.sin(c)
+            return Area
             
 
 def parallelogram(base, height):
@@ -50,3 +81,5 @@ def circle(radius):
 
 def sector(radius, angle):
     return math.pi * radius ** 2 *(angle/(2*math.pi))
+if __name__=="__main__":
+    print(solve_triangle(4,3,"?","?","?",(math.pi/2)))
