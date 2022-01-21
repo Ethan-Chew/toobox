24e650f8742dc0d06d3f08a124add412e0a97ef6
Jerick Seng <64419693+jer123se12@users.noreply.github.com>
2022-01-21 11:39:18 +0800

    debugging time

--- tools/Algebra culculator.py
+++ tools/Algebra culculator.py
@@ -1,5 +1,5 @@
 class algebric:
-	def __init__(self,number,algebric):
+	def __init__(self,number,algebric={}):
 		if number==0 and (algebric):
 			number=1
 		self.num=number
@@ -17,7 +17,7 @@ class calculator:
 			return False
 	def find_end_num(self,value):
 		#find the end of the number
-		print(value)
+		
 		number="F"
 		for i in range(len(value),0,-1):
 			if self.is_float(value[:i]):
@@ -29,10 +29,10 @@ class calculator:
 		else:
 			return number
 	def __init__(self):
-		self.tokens={"*":(lambda x,y: mul(x,y)) , "/":(lambda x,y: div(x,y)) , "+":(lambda x,y: add(x,y)) , "-":(lambda x,y: minus(x,y))  }
+		self.tokens={"*":(lambda x,y: self.mul(x,y)) , "/":(lambda x,y: self.div(x,y)) , "+":(lambda x,y: self.add(x,y)) , "-":(lambda x,y: self.minus(x,y))  }
 	def turn(self, strin):
 		# turns something like 1abc to a algebric type
-		print(strin)
+		
 		number=0.0
 		algebras={}
 		end=0
@@ -65,8 +65,9 @@ class calculator:
 	        if count==0:
 	            return i+1
 
-	def solve(self,stri):
+	def sol(self,stri):
 		parsed=self.parse(stri)
+		return self.solve(parsed)
 
 	def parse(self,stri):
 		#parseing the eqation
@@ -98,7 +99,7 @@ class calculator:
 			if len(parsed)>0 and type(parsed[-1])==list:
 				parsed.append("*")
 			parsed.append(self.turn(buf))
-			print(parsed)
+			
 		reparsed=[]
 		#deal with double paranthesis
 		if list in parsed:
@@ -112,10 +113,10 @@ class calculator:
 			parsed=reparsed
 		reparsed=[]
 		# turn negative numbers into 0-1
-		print(parsed)
+		
 		if parsed[0] in self.tokens:
 			parsed=[algebric(0,{})]+parsed
-		print(parsed)
+		
 		skip=0
 		for i in range(len(parsed)):
 			if skip!=0:
@@ -137,18 +138,124 @@ class calculator:
 					reparsed.append(self.solve(i))
 				else:
 					reparsed.append(i)
-		parsed=reparsed
+			parsed=reparsed
 		
 		#reduce the paranthesis to the simplest form
 		for i in range(len(parsed)):
 			if type(parsed[i]) == list:
 				parsed[i]=self.solve(parsed[i])
 		
-		while sum([ (x in parsed) for x in self.tokens])>0:
-			for j in tokens:
-				for i in range(parsed):
-					if parsed[i] == j:
-						return
+		#follow order of operation
+		final=[]
+		l=[]
+		for i in range(len(parsed)):
+			if i>0 and parsed[i-1]=="-":
+				l[-1]="+"
+				l.append(self.mul(parsed[i],algebric(-1)))
+			else:
+				l.append(parsed[i])
+		parsed=list(l)
+
+		for j in self.tokens:
+			i=0
+			reparsed=[]
+			while i<len(parsed):
+				cur=parsed[i]
+				if cur==j:
+					reparsed=reparsed[:-1]
+					reparsed+=(self.tokens[j](parsed[i-1],parsed[i+1]))
+					i+=1
+				else:
+					reparsed.append(cur)
+				i+=1
+			parsed=list(reparsed)
+
+
+
+				
+		return parsed
+	def mul(self,prev,nex):
+		print(prev,nex)
+		if type(prev)!=list:
+			prev=[prev]
+		if type(nex)!=list:
+			nex=[nex]
+		if len(prev)>1 or len(nex)>1:
+			# turn everything into addition
+			reprev=[]
+			for i in range(len(prev)):
+				if i>0 and prev[i-1]=="-":
+					reprev[-1]="+"
+					reprev.append(self.mul(prev[i],algebric(-1)))
+				else:
+					reprev.append(prev[i])
+			prev=list(reprev)
+			reprev=[]
+			for i in range(len(nex)):
+				if i>0 and nex[i-1]=="-":
+					reprev[-1]="+"
+					reprev.append(self.mul(nex[i],algebric(-1)))
+				else:
+					reprev.append(nex[i])
+			nex=list(reprev)
+			final=[]
+			for i in prev:
+				for j in nex:
+					if type(i)!=str and type(j)!=str:
+						final.append(self.minimul(i,j))
+						final.append("+")
+			final=final[:-1]
+			return self.solve(final)
+		else:
+			return [self.minimul(prev[0],nex[0])]
+
+	def minimul(self,prev,nex):
+		print(prev,nex)
+		number=prev.num*nex.num
+		algebra={}
+		for i in prev.al:
+			if i in nex.al:
+				algebra[i]=nex.al[i]+prev.al[i]
+			else:
+				algebra[i]=prev.al[i]
+		for i in nex.al:
+			if i in prev.al:
+				pass
+			else:
+				algebra[i]=nex.al[i]
+		return algebric(number,algebra)
+
+	def div(self,prev,nex):
+		if type(prev)!=list and type(nex)!=list:
+			return self.minidiv(prev,nex)
+		elif type(nex)==list:
+			# need to deal with factorisation
+			return [prev,nex]
+		else:
+			#invrese nex
+			nex=self.div(algebric(1),nex)
+			return self.mul(prev,nex)
+
+	def minidiv(self,prev,nex):
+		number=prev.num/nex.num
+		algebra={}
+		for i in prev.al:
+			if i in nex.al:
+				algebra[i]=nex.al[i]-prev.al[i]
+			else:
+				algebra[i]=prev.al[i]
+		for i in nex.al:
+			if i in prev.al:
+				pass
+			else:
+				algebra[i]= -nex.al[i]
+	def add(self,prev,nex):
+		if prev.al ==nex.al:
+			prev.num=prev.num+nex.num
+			return prev
+		else:
+			return [prev,"+",nex]
+	
 
 
 
@@ -160,8 +267,8 @@ class calculator:
 
 
 a=calculator()
-print("1+b*(a+b)")
-print(a.parse("1+b*-(a+b)"))
+
+print(a.sol("1+b*(a+b)"))
 
 
 
