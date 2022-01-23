613dacc9f78527230bf4deb2f1df37ac0045fc8e
granwyntan <granwyn_tan@s2019.ssts.edu.sg>
2022-01-23 20:59:03 +0800

    fixed, dun complain im still working on stuff

--- toolsUI.py
+++ toolsUI.py
@@ -46,7 +46,7 @@ def notUsable(self):
 def Rectangle(self):
     self.welcomeFrame = ttk.Frame(self.notebook)
     self.welcomeFrame.pack(side="top", padx=25, pady=18, anchor="w")
-    self.mainLabel = WrappingLabel(self.welcomeFrame, text="Chemical Equation Balancer", font=("TkDefaultFont",50,'bold'))
+    self.mainLabel = WrappingLabel(self.welcomeFrame, text="Rectangle Area Calculator", font=("TkDefaultFont",50,'bold'))
     self.mainLabel.pack(side="top", pady=2, fill="x", expand="yes")
     self.infoLabel = WrappingLabel(self.welcomeFrame, text="Please enter the Chemical Equation like in the following example: 'Compound(State) + Compound2(State) + ... -> Compound3(State) + Compound4(State) + ...'", font=("TkDefaultFont", 15))
     self.infoLabel.pack(side="top", pady=2, fill="x", expand="yes")
